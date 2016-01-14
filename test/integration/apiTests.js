8693ccf89963d99e51c8fd1aa3f0cf0f8c1925fd
ksiddana <ksiddana>
2016-01-13 19:17:00 -0800

    Integration Tests Progress

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,33 @@
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
+  before(function() {
+
+    server = sinon.fakeServer.create();
+
+  })
+
+  after(function () { 
+    server.restore(); 
+  });
 
   it('todo.setup receives an array of todos when todo.init is called', function () {
+    var callback = sinon.spy();
+    todo.api.sendRequest({ method: 'get', endpoint: 'http://localhost:3000/todos'}, callback);
+
+    // This is part of the FakeXMLHttpRequest API
+
+    console.log(server.requests[0].respond);
+      
+    server.requests[0].respond(200,
+      { "Content-Type": "application/json" },
+      JSON.stringify([1,2,3])
+    );
+
+    assert(callback.calledOnce);
+
   });
 });
--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -2,13 +2,13 @@ var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
 
-describe('the todo.App', function() {
-  describe('the todo object', function(){
+// describe('the todo.App', function() {
+//   describe('the todo object', function(){
 
-    it('should have all the necessary methods', function(){
-    });
-  });
-});
+//     it('should have all the necessary methods', function(){
+//     });
+//   });
+// });
 
-describe('the todo.util methods', function() {
-});
+// describe('the todo.util methods', function() {
+// });
